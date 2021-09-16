4f8ebb3a7f496f6fbcca85989216d743c49237d9
Juan <john.kuttai@gmail.com>
2021-09-16 15:44:14 -0700

    test3

--- SimPEG/dask/electromagnetics/static/resistivity/simulation.py
+++ SimPEG/dask/electromagnetics/static/resistivity/simulation.py
@@ -165,28 +165,28 @@ def dask_dpred(self, m, f=None, compute_J=False):
 
     Where P is a projection of the fields onto the data space.
     """
-    if self.survey is None:
-        raise AttributeError(
-            "The survey has not yet been set and is required to compute "
-            "data. Please set the survey for the simulation: "
-            "simulation.survey = survey"
-        )
+    # if self.survey is None:
+    #     raise AttributeError(
+    #         "The survey has not yet been set and is required to compute "
+    #         "data. Please set the survey for the simulation: "
+    #         "simulation.survey = survey"
+    #     )
 
-    if f is None:
-        if m is None:
-            m = self.model
-        f, Ainv = self.fields(m, return_Ainv=compute_J)
-    print(f)
-    data = Data(self.survey)
-    for src in self.survey.source_list:
-        for rx in src.receiver_list:
-            data[src, rx] = rx.eval(src, self.mesh, f)
+    # if f is None:
+    #     if m is None:
+    #         m = self.model
+    #     f, Ainv = self.fields(m, return_Ainv=compute_J)
+    # print(f)
+    # data = Data(self.survey)
+    # for src in self.survey.source_list:
+    #     for rx in src.receiver_list:
+    #         data[src, rx] = rx.eval(src, self.mesh, f)
 
-    if compute_J:
-        Jmatrix = self.compute_J(f=f, Ainv=Ainv)
-        return (mkvc(data), Jmatrix)
+    # if compute_J:
+    #     Jmatrix = self.compute_J(f=f, Ainv=Ainv)
+    #     return (mkvc(data), Jmatrix)
 
-    return mkvc(data)
+    return np.ones(self.survey.nD)
 
 
 Sim.dpred = dask_dpred
